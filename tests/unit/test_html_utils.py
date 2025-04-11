aa04e1c407d6d4b81c64e06b2752176394747b5e
ShaheeraMudasar <shaheeramudasar@gmail.com>
2025-04-11 11:20:45 +0200

    Divided the file in different sections

---
+++ tests/unit/test_html_utils.py
@@ -0,0 +1,26 @@
+# unit tests for functions: get_html_start_block, get_html_end_block
+
+
+
+
+
+
+
+
+
+# unit test for functions: to_heading_line, to_text_paragraph
+
+
+
+
+
+
+
+
+
+
+# unit test for functions: to_error_message, to_basic_html_page
+
+
+
+
