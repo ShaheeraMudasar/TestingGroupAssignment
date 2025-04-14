672719f2f45c0b4477448ca662c52cf02d8fd357
ShaheeraMudasar <shaheeramudasar@gmail.com>
2025-04-14 13:32:29 +0200

    just made some sections for everyone in tests_db.py file

--- tests/unit/tests_db.py
+++ tests/unit/tests_db.py
@@ -1,3 +1,37 @@
-# TestCase: add_visit should save a visit's detail in database using mock data 
-# and return with the visit info on homepage without errror
-# 
+# Unit tests for functions: add_visit (Tester: Mehdi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Unit tests for functions: get_all_visits (Tester: Detelina)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Unit tests for functions: get_visit_by_id (Tester: Shaheera)
