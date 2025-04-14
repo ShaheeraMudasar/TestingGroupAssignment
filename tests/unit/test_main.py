e29ac59a5baac035709fe2185a395fd017b3c3af
Detelina <detelinamarinova43@gmail.com>
2025-04-14 15:55:54 +0200

    added comments who does what

---
+++ tests/unit/test_main.py
@@ -0,0 +1,30 @@
+#def root() def visits() (Detelina)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def visit(visit_id) def hello() (Shaheera)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+#def hello_form()(Mehdi)
--- tests/unit/test_rendering.py
+++ tests/unit/test_rendering.py
@@ -2,6 +2,30 @@
 
 # def format_visit_history(history) (Mehdi)
 
-#def format_visit_details(visit) (Detelina)
+
+
+
+
+
+
+
+
+
+
+
+# def format_visit_details(visit) (Detelina)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #def format_hello_greeting(name) (Shaheera)
