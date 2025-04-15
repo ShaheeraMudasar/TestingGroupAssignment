b097711a6b62a97fa3aa717bf96b48941c0d53dc
Detelina <detelinamarinova43@gmail.com>
2025-04-15 16:18:06 +0200

    Added rendering tests

--- tests/unit/test_rendering.py
+++ tests/unit/test_rendering.py
@@ -1,3 +1,7 @@
+from unittest.mock import patch
+from html_utils import get_html_start_block, to_heading_line, to_text_paragraph, get_html_end_block
+from rendering import format_visit_details
+
 
 
 # def format_visit_history(history) (Mehdi)
@@ -15,8 +19,38 @@
 
 # def format_visit_details(visit) (Detelina)
 
+@patch("rendering.get_html_end_block", return_value="End")
+@patch("rendering.to_text_paragraph", return_value="Paragraph")
+@patch("rendering.to_heading_line", return_value="Heading")
+@patch("rendering.get_html_start_block", return_value="Start")
+def test_format_visit_details_normal(mock_start, mock_heading, mock_paragraph, mock_end ):
+    visit = {
+    "id": 1,
+    "timestamp": "2025-01-01 12:00:00",
+    "ip": "127.0.0.1",
+    "user_agent": "Chrome"
+}
+    result = format_visit_details(visit)
+    expected = "StartHeadingParagraphParagraphParagraphEnd"
+    assert result == expected
 
+  
+@patch("rendering.get_html_end_block", return_value="End")
+@patch("rendering.to_text_paragraph", return_value="Paragraph")
+@patch("rendering.to_heading_line", return_value="Heading")
+@patch("rendering.get_html_start_block", return_value="Start")
+def test_format_visit_details_missing_data(mock_start, mock_heading, mock_paragraph, mock_end ):
+    visit = {
+    "id": "2",
+    "timestamp": "2025-01-01 12:00:00",
+    "ip": "",
+    "user_agent": "Chrome"
+}
+    result = format_visit_details(visit)
+    expected = "StartHeadingParagraphParagraphParagraphEnd"
+    assert result == expected
 
+  
 
 
 
