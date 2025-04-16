af263ab117bc016ac734eb0dbd3b1fb1bcdf57f4
ShaheeraMudasar <shaheeramudasar@gmail.com>
2025-04-16 11:40:16 +0200

    added my part of tests in test_main file

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,6 +1,7 @@
-from main import app
-from main import root, visits, visit, hello, hello_form
-from hamcrest import assert_that, equal_to
+from main import app, root, visits, visit, hello, hello_form
+from hamcrest import assert_that, equal_to, contains_string
+from rendering import format_hello_greeting
+from unittest.mock import patch
 
 # Unit test for the functions: def root() def visits() (Detelina)
 
@@ -17,21 +18,46 @@ from hamcrest import assert_that, equal_to
 
 
 
-# Unit test for the functions: def visit(visit_id) def hello() (Shaheera)
- 
+# Unit test for the functions: def visit(visit_id) def hello() (Tester: Shaheera)
+@patch("main.get_visit_by_id", 
+       return_value = {"id": "1",
+       "timestamp": "2025-01-01", 
+       "ip": "127.0.0.1", "user_agent":"testAgent"})
+@patch("main.format_visit_history", return_value = "Visit details")
 
+# Test Case: When visit id exists 
+def test_visit_by_id_returns_detail_about_that_visit(mock_format, mock_get_visit): 
+    client = app.test_client()
 
+    # WHEN visit id is provided
+    response = client.get("/visit/1")
+    # THEN it should return visit details against that id
+    assert_that(response.get_data(as_text = True), contains_string("Visit details"))
 
+# Test case: When the visit id is not found
+@patch("main.get_visit_by_id", 
+       return_value = None)
+@patch("main.format_visit_history", return_value = "Visit not found")
 
+def test_visit_by_id_should_return_404_when_no_visit_found(mock_get_visit, mock_format):
+    client = app.test_client()
+    # WHEN an invalid id is provided
+    response = client.get ("/visit/999")
+    # THEN it throws a 404 error with a message
+    assert_that(response.status_code, equal_to(404))
+    assert_that(response.get_data(as_text = True), contains_string("Visit not found"))
 
+# Test Case: When a name is provided in the web-form
+@patch("main.format_hello_greeting", return_value = "Hello Alice!")
+def test_hello_returns_greetings(mock_format_hello_greeting):
+    client = app.test_client()
+    # WHEN name is provided 
+    response = client.get ("/hello?name=Alice")
+    # THEN greetings are displayed for that name
+    assert_that(response.get_data(as_text = True), contains_string("Hello Alice!"))
+    
 
-
-
-
-
-
-
-# Unit test for the function: def hello_form()(Mehdi)
+# Unit test for the function: def hello_form()(Tester: Mehdi)
 # The function hello_form() renders a simple static HTML form that lets users input their name and submit it.
 # It includes a test field for the name and a submit button.
 # We use Flask's built-in test_client() to simulate requests.
